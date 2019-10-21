09c81c7e727b356cd58849503e102d4e0aa50bd9
Maciel Barbosa <macielbarbosa@outlook.com>
2019-10-21 18:12:10 -0300

    Tempo de execução e grupo de dados

--- mongo-indices/index.js
+++ mongo-indices/index.js
@@ -1,26 +1,50 @@
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+const timer = require('execution-time')()
 
-const generateNumber = () => Math.floor(Math.random() * 101);
- 
-// Connection URL
-const url = 'mongodb://localhost:27017';
- 
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+const URL = 'mongodb://localhost:27017'
+const DB_NAME = 'mongo-indices'
 
-  const documents = Array.from({length: 10/*00000*/}, () => ({
-    val1: generateNumber(), val2: generateNumber()
+const gerarNumero = () => Math.floor(Math.random() * 101)
+const exibirTempo = resultado => { console.log(resultado.words) }
+
+const agruparDados = (dados, {quantidade}) => {
+  const dadosAgrupados = []
+  let posicaoInicial = 0
+  quantidadeGrupos = Math.ceil(dados.length/quantidade)
+  while (quantidadeGrupos--) {
+    const grupo = dados.slice(posicaoInicial, posicaoInicial + quantidade)
+    dadosAgrupados.push(grupo)
+    posicaoInicial += quantidade
+  }
+  return dadosAgrupados
+}
+
+const inserirDados = db => {
+  const documentos = Array.from({length: 10000000}, () => ({
+    val1: gerarNumero(), val2: gerarNumero()
   }))
+  const documentosAgrupados = agruparDados(documentos, {quantidade: 4})
+  //const collection = db.collection()
+}
 
-  console.log(documents)
+medirTempoExecucao = (func, nome) => {
+  timer.start(nome)
+  func()
+  console.log(`'${nome}' executado em: ${timer.stop(nome).words}`)
+}
  
-  //const db = client.db(dbName);
- 
-  /*insertDocuments(db, function() {
-    
-  });*/
-  client.close();
-});
+const app = (err, client) => {
+  if(err) {
+    console.log('Erro ao conectar ao servidor')
+    return
+  }
+  console.log("Conectado ao servidor")
+
+  const db = client.db(DB_NAME)
+  medirTempoExecucao(() => { inserirDados(db)}, 'Inserção de dados')
+  
+  client.close()
+}
+
+MongoClient.connect(URL, app)
