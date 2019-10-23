b7924c729526aa0b618226624a6737f3601d6ed2
Maciel Barbosa <macielbarbosa@outlook.com>
2019-10-23 12:16:49 -0300

    inserção de dados no mongo

--- mongo-indices/index.js
+++ mongo-indices/index.js
@@ -6,35 +6,49 @@ const URL = 'mongodb://localhost:27017'
 const DB_NAME = 'mongo-indices'
 
 const gerarNumero = () => Math.floor(Math.random() * 101)
-const exibirTempo = resultado => { console.log(resultado.words) }
 
-const agruparDados = (dados, {quantidade}) => {
+const agruparDados = (dados, { quantidadePorGrupo }) => {
   const dadosAgrupados = []
   let posicaoInicial = 0
-  quantidadeGrupos = Math.ceil(dados.length/quantidade)
+  quantidadeGrupos = Math.ceil(dados.length/quantidadePorGrupo)
   while (quantidadeGrupos--) {
-    const grupo = dados.slice(posicaoInicial, posicaoInicial + quantidade)
+    const grupo = dados.slice(posicaoInicial, posicaoInicial + quantidadePorGrupo)
     dadosAgrupados.push(grupo)
-    posicaoInicial += quantidade
+    posicaoInicial += quantidadePorGrupo
   }
   return dadosAgrupados
 }
 
-const inserirDados = db => {
-  const documentos = Array.from({length: 10000000}, () => ({
-    val1: gerarNumero(), val2: gerarNumero()
-  }))
-  const documentosAgrupados = agruparDados(documentos, {quantidade: 4})
-  //const collection = db.collection()
+const insertMany = (documentos, collection) => new Promise((resolve, reject) => {
+  collection.insertMany(documentos, erro => {
+    if (erro) reject(erro)
+    resolve()
+  })
+})
+
+const inserirDados = async (dados, collection) => {
+  for(grupo of dados) {
+    await insertMany(grupo, collection)
+  }
 }
 
-medirTempoExecucao = (func, nome) => {
+const gerarDados = quantidade => {
+  const documentos = Array.from({length: quantidade}, () => ({
+    val1: gerarNumero(), val2: gerarNumero()
+  }))
+  return agruparDados(documentos, { quantidadePorGrupo: 10000 })
+}
+
+const medirTempoExecucao = async (funcao, nome) => {
+  console.log(`Iniciando '${nome}' ...`)
   timer.start(nome)
-  func()
-  console.log(`'${nome}' executado em: ${timer.stop(nome).words}`)
+  await funcao()
+  console.log('---------------------------------------------------------')
+  console.log(`'${nome}' executado em ${timer.stop(nome).words}`)
+  console.log('---------------------------------------------------------')
 }
  
-const app = (err, client) => {
+const app = async (err, client) => {
   if(err) {
     console.log('Erro ao conectar ao servidor')
     return
@@ -42,7 +56,15 @@ const app = (err, client) => {
   console.log("Conectado ao servidor")
 
   const db = client.db(DB_NAME)
-  medirTempoExecucao(() => { inserirDados(db)}, 'Inserção de dados')
+  const collection = db.collection('documentos')
+  console.log('Gerando dados ...')
+  const dados = gerarDados(1000000)
+
+  await medirTempoExecucao(() => inserirDados(dados, collection), 'Inserção de dados sem indexação')
+  // await medirTempoExecucao(() => inserirDados(dados, collection), 'Consulta por valores em val1 entre 0 e 10')
+
+  console.log('Apagando todos os documentos ...')
+  db.collection('documentos').drop()
   
   client.close()
 }
